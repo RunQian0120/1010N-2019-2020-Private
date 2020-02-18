cc86d3fdb7312294000e70a9f03dda36aea5331c
RunQian0120 <43157084+RunQian0120@users.noreply.github.com>
2020-02-18 15:04:54 -0800

    oj

---
+++ TripleAtProvs/src/autonomous.cpp
@@ -0,0 +1,168 @@
+#include "main.h"
+
+
+std::shared_ptr<OdomChassisController> chassis = ChassisControllerBuilder()
+    .withMotors({flPort, blPort}, {frPort, brPort})
+
+    .withDimensions(AbstractMotor::gearset::green, {{4_in, 10_in}, imev5GreenTPR})
+    .withSensors(okapi::ADIEncoder{'B', 'C'}, okapi::ADIEncoder{'D', 'E', true})
+    .withOdometry({{2.75_in, 10.4_in}, quadEncoderTPR}, StateMode::FRAME_TRANSFORMATION) // use the same scales as the chassis (above)
+    .buildOdometry(); // build an odometry chassis
+
+void flipOut() {
+
+}
+
+
+void blueProtected() {
+    flipOut();
+    lift.setIntakePower(127);
+
+    base.pidDrive(-1, 400, 1200, 127);
+    base.pidDrive(1, 200, 1000, 127);
+    base.pidStrafe(-1, 700, 1000, 127);
+    base.pidDrive(1, 500, 1000, 127);
+    base.pidStrafe(1, 200, 700, 127);
+    base.pidDrive(1, 500, 1000, 127);
+    base.pidTurn(-1, 350, 1000, 127);
+    base.pidDrive(1, 1300, 1200, 127);
+    base.pidTurn(-1, 650, 1200, 127);
+    base.pidDrive(1, 1500, 3000, 70);
+}
+
+void redProtected() {
+  flipOut();
+  lift.setIntakePower(127);
+
+  base.pidDrive(-1, 500, 1200, 127);
+  base.pidDrive(1, 200, 1000, 127);
+  base.pidStrafe(1, 700, 1000, 127);
+  base.pidDrive(1, 500, 1000, 127);
+  base.pidStrafe(-1, 200, 700, 127);
+  base.pidDrive(1, 500, 1000, 127);
+  base.pidTurn(1, 350, 1000, 127);
+  base.pidDrive(1, 1300, 1200, 127);
+  base.pidTurn(1, 650, 1200, 127);
+  base.pidDrive(1, 1500, 3000, 70);
+}
+
+void redUnprotected() {
+  flipOut();
+
+  lift.setIntakePower(127);
+
+  //autoTopCube = true;
+  base.pidDrive(1, 2000, 3500, 90);
+  pros::delay(50);
+  lift.movePos(700, 500);
+  base.pidDrive(1, 500, 500, 90);
+  resetLift = true;
+  pros::delay(500);
+  base.pidTurn(left, 550, 1000,127);
+
+  base.lineUp(3000, 100);
+}
+
+void blueUnprotected() {
+    flipOut();
+
+    lift.setIntakePower(127);
+
+      /*
+        base.pidDrive(1, 800, 1400, 127);
+        base.pidDrive(-1, 900, 1400, 127);
+        //base.pidDrive(1, 50, 00, 127);
+        base.pidStrafe(-1, 1000, 1800, 127);
+        base.pidDrive(-1, 250, 500, 127);
+
+        */
+    base.pidDrive(1, 1000, 2000, 80);
+
+      //  base.pidTurn(1, 200, 1000, 127);
+
+      //  base.pidDrive(1, 300, 1000, 127);
+    //    base.pidTurn(-1, -200, 1000, 127);
+
+    base.pidDrive(-1, 950, 1000, 127);
+      //  base.pidDrive(-1, 200, 500, 90);
+    lift.setIntakePower(0);
+    base.pidTurn(1, 440, 1000, 127);
+
+    base.pidStrafe(1, 650, 750, 127);
+    base.pidDrive(1, 200, 500, 127);
+    multiPlace = true;
+    pros::delay(3200);
+    base.pidDrive(-1, 500, 1000, 127);
+}
+
+
+
+void moveLiftPos(int speed, int timeOut) {
+  //LiftM.tare_position();
+  int timeStamp = pros::millis();
+  int netTime = 0;
+  while(netTime < timeOut) {
+    netTime = pros::millis() - timeStamp;
+    LiftM = speed;
+    //delay(50);
+  }
+  LiftM = 0;
+}
+
+
+void skillsAuto() {
+  flipOut();
+
+//  slowOutTake = true;
+  lift.movePos(1495, 1000);
+  base.pidDrive(forward, 850, 1500, 127);
+  lift.setIntakePower(-90);
+  pros::delay(800);
+  base.lineUp(2000, 127);
+}
+
+
+void autonomous() {
+
+  redUnprotected();
+  //base.pidTurn(1, 1000, 10000, 127);
+
+  //base.pidIMUTurn(left, 270, 1000, 127);
+//  redUnprotected();
+//  moveLiftPos(-127, 1000);
+/*
+  base.pidDrive(1, 250, 1000, 127);
+  LiftM = -127;
+  pros::delay(1000);
+  LiftM = 127;
+  pros::delay(1400);
+  LiftM = 0;
+
+  lift.setIntakePower(127);
+
+
+  base.pidDrive(1, 800, 1400, 127);
+  base.pidDrive(-1, 900, 1400, 127);
+  //base.pidDrive(1, 50, 00, 127);
+  base.pidStrafe(-1, 1000, 1800, 127);
+  base.pidDrive(-1, 250, 500, 127);
+
+
+  base.pidDrive(1, 1100, 1700, 90);
+
+
+  base.pidDrive(-1, 950, 1500, 127);
+  lift.setIntakePower(0);
+  base.pidTurn(-1, 400, 1000, 127);
+  multiPlace = true;
+  base.pidStrafe(-1, 600, 1100, 127);
+  base.pidDrive(1, 200, 1500, 127);
+  pros::delay(1200);
+  base.pidDrive(-1, 500, 1000, 127);
+
+*/
+
+  //  moveLiftPos(-127, 1000);
+
+
+}
